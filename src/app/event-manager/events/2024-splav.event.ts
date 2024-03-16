0eb47bd3bbf1392706fd156ada333c11867d44d4
michalmazak <michal.mazak@promako.sk>
2024-03-16 17:01:30 +0100

    swap survey order

--- src/app/event-manager/events/2024-splav.event.ts
+++ src/app/event-manager/events/2024-splav.event.ts
@@ -5,25 +5,6 @@ export const splav2024: Event = {
   id: '2024-splav',
   title: 'Splav 2024',
   questions: ([{
-    id: 'pocetdni',
-    title: 'Na ako dlho chceš ísť?',
-    description: 'Chodíme iba raz za rok a 3 noci sú viac srandy ako 2. Ten posledný deň však býva dosť náročný.',
-    type: 'radio',
-    dontcare: 'true',
-    choices: [{
-      id: 'pocetdni4',
-      text: '3 noci',
-      description: 'Streda večer prídeme, Štvrtok ráno štart, Nedeľa večer doma.'
-    }, {
-      id: 'pocetdni4aleposlednydennesplavovat',
-      text: '3 noci*',
-      description: '...ale v Nedeľu už nesplavovať, ráno hneď domov.'
-    }, {
-      id: 'pocetdni3',
-      text: '2 noci',
-      description: 'Štvrtok večer prídeme, Piatok ráno štart, Nedeľa večer doma.'
-    }]
-  }, {
     id: 'termin',
     title: 'Ktoré víkendy môžeš ísť?',
     description: 'Fakt vyber prosím čo najviac, aby sme sa mali šancu zhodnúť. Nie že kedy sa ti chce.',
@@ -73,6 +54,25 @@ export const splav2024: Event = {
       text: '17. - 18. Aug',
       description: 'Lovestream',
       selected: true
+    }]
+  }, {
+    id: 'pocetdni',
+    title: 'Na ako dlho chceš ísť?',
+    description: 'Chodíme iba raz za rok a 3 noci sú viac srandy ako 2. Ten posledný deň však býva dosť náročný.',
+    type: 'radio',
+    dontcare: 'true',
+    choices: [{
+      id: 'pocetdni4',
+      text: '3 noci',
+      description: 'Streda večer prídeme, Štvrtok ráno štart, Nedeľa večer doma.'
+    }, {
+      id: 'pocetdni4aleposlednydennesplavovat',
+      text: '3 noci*',
+      description: '...ale v Nedeľu už nesplavovať, ráno hneď domov.'
+    }, {
+      id: 'pocetdni3',
+      text: '2 noci',
+      description: 'Štvrtok večer prídeme, Piatok ráno štart, Nedeľa večer doma.'
     }]
   }] as SurveyQuestion[]).map(poll => {
     if (poll.dontcare) poll.choices.push({ id: 'dontcare', text: 'Je mi to jedno', selected: !poll.choices.some(choice => choice.selected) });
