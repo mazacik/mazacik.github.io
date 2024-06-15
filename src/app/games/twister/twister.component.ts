2b8d86f18986974a97dbac58d44fc39a0e09526a
michalmazak <michal.mazak@promako.sk>
2024-06-15 15:19:27 +0200

    add twister

---
+++ src/app/games/twister/twister.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RandomUtils } from 'src/app/shared/utils/random.utils';
+
+@Component({
+  selector: 'app-twister',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './twister.component.html',
+  styleUrls: ['./twister.component.scss']
+})
+export class TwisterComponent implements OnInit {
+
+  private limbs: string[] = ['pravá ruka', 'ľavá ruka', 'pravá noha', 'ľavá noha'];
+  private colors: string[][] = [['červená', 'firebrick'], ['zelená', 'limegreen'], ['modrá', 'blue'], ['žltá', 'yellow']];
+
+  protected limb: string;
+  protected color: string;
+  protected backgroundColor: string;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.spin();
+  }
+
+  protected spin(): void {
+    this.limb = RandomUtils.from(this.limbs, this.limb);
+    [this.color, this.backgroundColor] = RandomUtils.from(this.colors);
+  }
+
+}
