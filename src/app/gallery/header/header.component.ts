1eb221cbc9d5da550c23de3ad80b259964f9246e
michalmazak <michal.mazak@promako.sk>
2025-05-05 23:38:37 +0200

    show active tags on header

--- src/app/gallery/header/header.component.ts
+++ src/app/gallery/header/header.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { TippyDirective } from '@ngneat/helipopper';
 import { ApplicationService } from 'src/app/shared/services/application.service';
 import { GalleryService } from '../gallery.service';
+import { TagGroup } from '../model/tag-group.interface';
+import { Tag } from '../model/tag.interface';
 import { GalleryStateService } from '../services/gallery-state.service';
 
 @Component({
@@ -22,5 +24,23 @@ export class HeaderComponent {
     protected stateService: GalleryStateService,
     protected galleryService: GalleryService
   ) { }
+
+  protected resetTag(tag: Tag): void {
+    tag.state = 0;
+    this.stateService.updateFilters();
+    this.stateService.save();
+  }
+
+  protected getPositiveTags(): Tag[] {
+    return this.stateService.tagGroups?.flatMap(group => group.tags).filter(tag => tag.state == 1);
+  }
+
+  protected getNegativeTags(): Tag[] {
+    return this.stateService.tagGroups?.flatMap(group => group.tags).filter(tag => tag.state == -1);
+  }
+
+  protected getTagGroup(tag: Tag): TagGroup {
+    return this.stateService.tagGroups.find(group => group.tags.includes(tag));
+  }
 
 }
